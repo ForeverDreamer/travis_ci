11de6e9e04fac082bf816e80c750fa73223f1589
doer <499361328@qq.com>
2019-05-11 11:39:03 +0800

    初始化提交

---
+++ build_scripts/start_message.js
@@ -0,0 +1,6 @@
+// ES5(2009)语法, CommonJS Module Format(Used by node)
+// let chalk = require('chalk');
+
+// ES6(2015)语法, ES6 Module Format
+import chalk from 'chalk';
+console.log(chalk.green('Starting app in dev mode...'));
